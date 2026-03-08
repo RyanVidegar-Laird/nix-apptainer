223dc84d73ee23f148c69aac4a0d65b332add599
ryanvl <rvl-git@tuta.io>
2026-03-07 23:42:55 -0500

    feat: add aarch64-linux support

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   outputs =
     inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      systems = [ "x86_64-linux" "aarch64-linux" ];
 
       perSystem =
         { pkgs, system, ... }:
