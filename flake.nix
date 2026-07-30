4062afafbe65863e1a6285d8eded9f7da1ce6fe8
ryanvl <rvl-git@tuta.io>
2026-07-30 13:14:36 -0400

    chore: bump nixpkgs to 26.05, update flake inputs

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
