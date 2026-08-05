e17e611cdcad458992b67910c0e86c254643d11b
ryanvl <rvl-git@tuta.io>
2026-08-05 10:54:26 -0400

    chore: bump version to 0.8.0

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       perSystem =
         { pkgs, system, ... }:
         let
-          version = "0.7.1";
+          version = "0.8.0";
 
           nixos = nixpkgs.lib.nixosSystem {
             inherit system;
